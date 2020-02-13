5edc73bfdddcc477bb76b26d7756b2e645d50d4d
Mateus Talles <mateustalles@gmail.com>
2020-02-13 20:16:33 -0300

    Updates

---
+++ precache-manifest.792cc0b58b72d4d791eb4a6b4c95da9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31b670b139a9ad4ee4ed358c84aefa8c",
+    "url": "/movie-library/index.html"
+  },
+  {
+    "revision": "be53b830cf37903a5f2b",
+    "url": "/movie-library/static/css/main.4fd9617b.chunk.css"
+  },
+  {
+    "revision": "4f63e2c86d2337f5227c",
+    "url": "/movie-library/static/js/2.cbead026.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/movie-library/static/js/2.cbead026.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be53b830cf37903a5f2b",
+    "url": "/movie-library/static/js/main.4a37e60c.chunk.js"
+  },
+  {
+    "revision": "b00e941f9d44f1160133",
+    "url": "/movie-library/static/js/runtime-main.8fedccff.js"
+  }
+]);
