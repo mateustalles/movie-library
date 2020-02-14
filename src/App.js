57ee416875d6a48a7e7ee862e8276cbfc3fbee02
Mateus Talles <mateustalles@gmail.com>
2020-02-14 01:39:26 -0300

    deploy

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ class App extends Component{
     return (
       <Router>
         <Switch>
-          <Route path="/movies/:id/edit" component={EditMovie} />
-          <Route path="/movies/new" component={NewMovie} />
-          <Route path="/movies/:id" component={MovieDetails} />
+          <Route path="/movies/:id/edit/" component={EditMovie} />
+          <Route path="/movies/new/" component={NewMovie} />
+          <Route path="/movies/:id/" component={MovieDetails} />
           <Route exact path="/" component={MovieList} />
           <Route path="*" render={NotFound} />
         </Switch>
