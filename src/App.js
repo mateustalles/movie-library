ff80d282faea0480b52644b51ef93453d2068741
Mateus Talles <mateustalles@gmail.com>
2020-02-13 23:37:53 -0300

    App now is a class

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import {
   MovieList, NewMovie, EditMovie, NotFound, MovieDetails,
 } from './pages';
 import './App.css';
 
-function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/movies/:id/edit" component={EditMovie} />
-        <Route path="/movies/new" component={NewMovie} />
-        <Route path="/movies/:id" component={MovieDetails} />
-        <Route exact path="/" component={MovieList} />
-        <Route path="*" render={NotFound} />
-      </Switch>
-    </Router>
-  );
+class App extends Component{
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/movies/:id/edit" component={EditMovie} />
+          <Route path="/movies/new" component={NewMovie} />
+          <Route path="/movies/:id" component={MovieDetails} />
+          <Route exact path="/" component={MovieList} />
+          <Route path="*" render={NotFound} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
